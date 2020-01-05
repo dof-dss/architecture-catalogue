4a6dee1ae0961ad12ede148925cccaa8b6882468
Stephen <patterson@Stephens-MBP.broadband>
2020-01-05 18:56:02 +0000

    Fix bug in textarea component

--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -20,8 +20,6 @@
         <label class="govuk-label" for="{{ $name }}">
             {{ $label }}
         </label>
-        <textarea class="govuk-textarea govuk-input--error" id="{{ $name }}" name="{{ $name }}" rows="5">
-            {{ $value ?? '' }}
-        </textarea>
+        <textarea class="govuk-textarea govuk-input--error" id="{{ $name }}" name="{{ $name }}" rows="5">{{ $value ?? '' }}</textarea>
     </div>
 @enderror
