4efb4203144a8a6664196d37f4e5077662b973d3
Stephen Patterson <stephenpatterson@stephens-imac.home>
2020-01-17 19:16:00 +0000

    Fix mismatch in Google ids

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -31,7 +31,7 @@
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
   <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-150837477-2"></script>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-150837477-3"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
