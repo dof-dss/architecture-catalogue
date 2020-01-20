39a7e359323d9692c0ff80ff66e0dd4d153182b6
Stephen Patterson <stephenpatterson@stephens-imac.home>
2020-01-20 20:05:46 +0000

    Add route logging for debug purposes

---
+++ app/Http/Middleware/LogRoute.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+use Illuminate\Support\Facades\Log;
+
+class LogRoute
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        Log::info($request->fullUrl());
+
+        return $next($request);
+    }
+}
