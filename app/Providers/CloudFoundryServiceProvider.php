73f197e2eebce5c73e773be8fb3bc0b0043ad1e6
Stephen Patterson <stephenpatterson@stephens-imac.home>
2020-02-10 23:19:03 +0000

    Update cloudfoundry settings and ensure PHP 7.3 is used

--- app/Providers/CloudFoundryServiceProvider.php
+++ app/Providers/CloudFoundryServiceProvider.php
@@ -21,21 +21,35 @@ class CloudFoundryServiceProvider extends ServiceProvider
             // Decode the JSON provided by Cloud Foundry.
             $config = json_decode(env('VCAP_SERVICES'), true);
 
-            // Set the MySQL config.
+            // Set the MySQL config
             $mysqlConfig = $config['mysql'][0]['credentials'];
             Config::set('database.connections.mysql.host', $mysqlConfig['host']);
             Config::set('database.connections.mysql.port', $mysqlConfig['port']);
             Config::set('database.connections.mysql.database', $mysqlConfig['name']);
             Config::set('database.connections.mysql.username', $mysqlConfig['username']);
             Config::set('database.connections.mysql.password', $mysqlConfig['password']);
+            // log settings
             Log::debug('Database name: ' . $mysqlConfig['name']);
             Log::debug('Database user: ' . $mysqlConfig['username']);
             Log::debug('Database password: ' . $mysqlConfig['password']);
 
-            // Set the elasticsearch config.
+            // Set the elasticsearch config
             $esConfig = $config['elasticsearch'][0]['credentials'];
-            Config::set('elasticquent.config.hosts', $esConfig('hostname') . ':' . $esConfig('port'));
-            Log::debug('Elasticsearch hosts: ' . config('elasticquent.config.hosts'));
+            $hosts = [
+                [
+                    'host' => $esConfig['hostname'],
+                    'port' => $esConfig['port'],
+                    'scheme' => 'https',
+                    'user' => $esConfig['username'],
+                    'pass' => $esConfig['password']
+                ]
+            ];
+            Config::set('elasticquent.config.hosts', $hosts);
+            //  log settings
+            Log::debug('Elasticsearch host: ' . $esConfig['hostname']);
+            Log::debug('Elasticsearch port: ' . $esConfig['port']);
+            Log::debug('Elasticsearch user: ' . $esConfig['username']);
+            Log::debug('Elasticsearch password: ' . $esConfig['password']);
         }
     }
 }
