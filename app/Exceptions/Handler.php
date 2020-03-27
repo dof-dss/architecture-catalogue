6cb556e04e8441cdb6186a3e41d994bca9e4192c
Stephen Patterson <stephenpatterson@stephens-imac.home>
2020-03-27 16:10:02 +0000

    Refine handler message. See #20.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -36,10 +36,10 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
-        Log::alert(
-            'Exception raised: ' . get_class($exception) .
-            ' - ' . $exception->getMessage()
-        );
+        Log::alert('Exception raised: ', [
+            'class' => get_class($exception),
+            'message' => $exception->getMessage()
+        ]);
         parent::report($exception);
     }
 
