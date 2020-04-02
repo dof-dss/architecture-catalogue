2ee6d88dcc2b570842c07d24d208414ec80f8e58
Stephen Patterson <stephenpatterson@stephens-imac.home>
2020-04-02 14:50:53 +0100

    This closes #34, #42, #43, #46.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -14,13 +14,27 @@ abstract class TestCase extends BaseTestCase
     use CreatesApplication;
     use RefreshDatabase;
 
-    public function loginAsFakeUser($persistUser)
+    /**
+     * Helper function
+     *
+     * @param boolean $persistUser
+     * @param string $role
+     * @param boolean $admin
+     * @return void
+     */
+    public function loginAsFakeUser($persistUser, $role = 'reader', $admin = false)
     {
         // create a user and login
         if ($persistUser) {
-            $user = factory(User::class)->create();
+            $user = factory(User::class)->create([
+                'role' => $role,
+                'admin' => $admin
+            ]);
         } else {
-            $user = factory(User::class)->make();
+            $user = factory(User::class)->make([
+                'role' => $role,
+                'admin' => $admin
+            ]);
         }
         $this->actingAs($user);
         return $user;
