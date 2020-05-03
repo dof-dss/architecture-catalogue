5c4b254cc2de991528ca7b5960591970e8cb616f
Stephen Patterson <stephenpatterson@stephens-imac.home>
2020-05-03 12:39:48 +0100

    Closes #135

--- tests/Feature/Entry/ViewEntryTest.php
+++ tests/Feature/Entry/ViewEntryTest.php
@@ -62,4 +62,46 @@ class ViewEntryTest extends TestCase
             ->get('/entries/1')
             ->assertSee('Back to search results');
     }
+
+
+    /**
+     * Check a contributor can't see a back link when viewing an entry editing
+     *
+     * @return void
+     */
+    public function testContributorCantSeeBackLinkWhenViewingAnEntryAfterEditing()
+    {
+        // stops notification being physically sent when a user is created
+        Notification::fake();
+
+        $user = $this->loginAsFakeUser(false, 'contributor', $visualCheck = 'assertSee');
+
+        // stops events being fired (i.e. will prevent audit)
+        Event::fake();
+
+        // mock up an a number of entries
+        factory(Entry::class, 125)->create();
+        // fetch the entry with an id of 1
+        $entry = Entry::find(1);
+
+        $this->followingRedirects()
+          ->from('/entries/' . $entry->id . '/edit')
+          ->post('/entries/' . $entry->id, [
+              '_method' => 'PUT',
+              'id' => $entry->id,
+              'name' => 'UPDATED',
+              'version' => $entry->version,
+              'description' => $entry->description,
+              'href' => $entry->href,
+              // category and sub_category are sent through from the UI like this
+              'category_subcategory' => $entry->category . "-" . $entry->sub_category,
+              'status' => $entry->status,
+              'functionality' => $entry->functionality,
+              'service_levels' => $entry->service_levels,
+              'interfaces' => $entry->interfaces
+          ])
+          ->assertSuccessful()
+          ->assertDontSee('Back to browse catalogue')
+          ->assertDontSee('Back to search results');
+    }
 }
