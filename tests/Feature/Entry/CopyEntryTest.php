e883b683b2a48081ce7e05499f138f2f21ba45e1
Stephen Patterson <stephenpatterson@stephens-imac.home>
2020-05-06 17:49:52 +0100

    Relates to #103

--- tests/Feature/Entry/CopyEntryTest.php
+++ tests/Feature/Entry/CopyEntryTest.php
@@ -11,6 +11,9 @@ use Illuminate\Support\Facades\Event;
 
 use App\User;
 use App\Entry;
+use App\Tag;
+use App\EntryTag;
+use App\Link;
 
 class CopyEntryTest extends TestCase
 {
@@ -29,18 +32,58 @@ class CopyEntryTest extends TestCase
 
         $user = $this->loginAsFakeUser(true, 'contributor');
 
-
-
         // create an entry
-        $entry = factory(Entry::class)->create([
-            'name' => 'AWS S3'
+        $entry1 = factory(Entry::class)->create([
+            'name' => 'Entry 1'
+        ]);
+
+        // create two other entries
+        $entry2 = factory(Entry::class)->create([
+            'name' => 'Entry 2'
+        ]);
+        $entry3 = factory(Entry::class)->create([
+            'name' => 'Entry 3'
+        ]);
+
+        // create the last two entries as dependencies of the first entry
+        Link::create([
+            'item1_id' => $entry1->id,
+            'item2_id' => $entry2->id,
+            'relationship' => 'composed_of'
+        ]);
+        Link::create([
+            'item1_id' => $entry1->id,
+            'item2_id' => $entry3->id,
+            'relationship' => 'composed_of'
+        ]);
+
+        // create two user defined tags
+        $tag1 = Tag::create([
+            'name' => 'tag1'
+        ]);
+        $tag2 = Tag::create([
+            'name' => 'tag2'
+        ]);
+
+        // link the two user defined tags to the first entry
+        EntryTag::create([
+            'entry_id' => $entry1->id,
+            'tag_id' => $tag1->id
+        ]);
+        EntryTag::create([
+            'entry_id' => $entry1->id,
+            'tag_id' => $tag2->id
         ]);
 
         // now copy it
         $this->followingRedirects()
-            ->from('/entries/' . $entry->id)
-            ->get('/entries/' . $entry->id . '/copy')
+            ->from('/entries/' . $entry1->id)
+            ->get('/entries/' . $entry1->id . '/copy')
             ->assertSuccessful()
-            ->assertSee(' AWS S3 - COPY');
+            ->assertSee(' Entry 1 - COPY')
+            ->assertSee('tag1')
+            ->assertSee('tag2')
+            ->assertSee('Entry 2')
+            ->assertSee('Entry 3');
     }
 }
