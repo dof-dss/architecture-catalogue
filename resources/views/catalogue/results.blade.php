e7f6a9a07d03fe17e74a693ef54450ae67557747
Stephen Patterson <stephenpatterson@stephens-imac.home>
2020-05-10 11:18:23 +0100

    Relates to #126

--- resources/views/catalogue/results.blade.php
+++ resources/views/catalogue/results.blade.php
@@ -4,17 +4,26 @@
 <h1 class="govuk-heading-l govuk-!-margin-bottom-1">Search results</h1>
 <span class="govuk-caption-m govuk-!-margin-bottom-2">Your search returned {{ $entries->count() . ' catalogue '. Str::plural('entry', $entries->count()) }}.</span>
 @if ($entries->count() > 0)
-  @include('partials.entries-table')
+    @include('partials.entries-table')
 @else
-  <div class="govuk-warning-text">
-      <span class="govuk-warning-text__icon" aria-hidden="true">!</span>
-      <strong class="govuk-warning-text__text">
-          <span class="govuk-warning-text__assistive">Warning</span>
-          There are no entries in the catalogue matching your search criteria.
-      </strong>
-  </div>
+    <div class="govuk-warning-text">
+        <span class="govuk-warning-text__icon" aria-hidden="true">!</span>
+        <strong class="govuk-warning-text__text">
+            <span class="govuk-warning-text__assistive">Warning</span>
+            There are no entries in the catalogue matching your search criteria.
+        </strong>
+    </div>
+    @if (auth()->user()->isContributor())
+        <a class="govuk-button govuk-button  govuk-!-margin-right-1" data-module="govuk-button" href="{{ route('entry.create') }}">
+            Add a new entry
+        </a>
+        <a class="govuk-button govuk-button--secondary" data-module="govuk-button" href="/entries/search">
+            Start a new search
+        </a>
+    @else
+        <a class="govuk-button govuk-button" data-module="govuk-button" href="/entries/search">
+            Start a new search
+        </a>
+    @endif
 @endif
-<a class="govuk-button govuk-button--secondary" data-module="govuk-button" href="/entries/search">
-    Start a new search
-</a>
 @endsection
