ff3ef3ecdadd00d6accc89928e4ceed314b8761f
Stephen Patterson <stephenpatterson@stephens-imac.home>
2020-05-11 21:20:08 +0100

    Tidy commit and tag messages

--- bump-version.sh
+++ bump-version.sh
@@ -66,8 +66,8 @@ if [ -f VERSION ]; then
     read
     echo -e "$PUSHING_MSG"
     git add CHANGELOG.md VERSION
-    git commit -m "Bump version to ${INPUT_STRING}."
-    git tag -a -m "Tag version ${INPUT_STRING}." "v$INPUT_STRING"
+    git commit -m "Bump version to ${INPUT_STRING}"
+    git tag -a -m "Tag version ${INPUT_STRING}" "v$INPUT_STRING"
     git push origin --tags
 else
     echo -e "${WARNING_FLAG} Could not find a VERSION file."
