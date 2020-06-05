aa8bd127d63e49d93a1f850a8953d883b42b185c
Stephen Patterson <stephenpatterson@stephens-imac.home>
2020-06-05 13:26:05 +0100

    Relates to #187

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <div class="">
     <h1 class="govuk-heading-xl">Architecture Catalogue</h1>
     <p class="govuk-body">
-        This architecture catalogue contains all of the solution building blocks (physical components) used by the NICS. This list is used to populate the NICS Architecture Portal which provides a publically accessible reference for architects designing solutions for the NICS.
+        This architecture catalogue contains all of the solution building blocks (physical components) used by the NICS. This list is used to populate the NICS Architecture Portal which provides a publicly accessible reference for architects designing solutions for the NICS.
     </p>
     <p class="govuk-body">
         Each entry has a status associated with it, which will determine whether or not it should form part of your solution architecture. If a component is not listed in the catalogue then it is not considered suitable for use in NICS solutions. If you believe the component should form part of the catalogue then please ask a user with the <i>contributor</i> role to add it for you or <a href="mailto:ea-team@ea.finance-ni.gov.uk" class = "govuk-link">contact the EA Team</a>.
