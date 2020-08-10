6d87665ab37febbb7ed4f258677ebd9a01ed11b2
Stephen Patterson <stephenpatterson@stephens-imac.home>
2020-08-10 12:07:37 +0100

    Default Swagger server to sandbox

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -232,6 +232,6 @@ return [
     |--------------------------------------------------------------------------
      */
     'constants' => [
-        'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://my-default-host.com'),
+        'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'https://architecture-catalogue-sandbox.london.cloudapps.digital/api/v1'),
     ],
 ];
